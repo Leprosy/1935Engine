03ed5a9adade2dc5d9f42989e5f09fe6468f82bd
Miguel Rojas <miguel_rojas@mentor.com>
2019-05-07 13:58:44 -0400

    Loading state uses labels and loader callbacks

--- jsSource/states/load.js
+++ jsSource/states/load.js
@@ -6,16 +6,27 @@ GAME.State.add("load", {
         // Init canvas
         GAME.Canvas.init($("#screen")[0]);
 
+        // A label
+        var text = GAME.Canvas.addText("Loading...", 40, 40, {
+            fontFamily: 'Arial',
+            fontSize: 36,
+            fontWeight: 'bold',
+            fill: ['#cccccc', '#000000'], // gradient
+            stroke: '#ffffff',
+            strokeThickness: 2});
+
         // Loader for assets, go to main_menu
         GAME.Load.list({files: ["img/player.png", "img/demo-player.png", "img/demo-bg-back.png",
                                 "img/demo-bg-middle.png","img/demo-bg-front.png"],
-                        progress: function(a, b, c) {
-                            console.debug("loading...");
+                        progress: function(ev, elem) {
+                            text.text = `Loading...${ev.progress}%`;
                         },
                         error: function(a, b, c) { // Error callback
-                            console.debug("error loading...");
+                            console.debug("Error loading...");
                         },
                         finish: function() { // End callback
+                            text.text = "Press Space";
+
                             GAME.Key.add("Space", function(ev) {
                                 GAME.State.set("demo");
                             });
